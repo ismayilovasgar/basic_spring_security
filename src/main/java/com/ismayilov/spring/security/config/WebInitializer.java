bb74c83ef61a88984d32f12f7ec3e622f57832e0
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-11-01 00:04:30 +0400

    start best practices about spring security

--- src/main/java/com/ismayilov/spring/security/config/WebInitializer.java
+++ src/main/java/com/ismayilov/spring/security/config/WebInitializer.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{ApplicationSecurityConfig.class};
+        return new Class[]{ApplicationConfig.class, ApplicationSecurityConfig.class};
     }
 
     @Override
